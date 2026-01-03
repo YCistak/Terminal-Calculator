acd1800288c5c9739f2f56d7a6d910c7e2f5dba0
YazilimciNuri <141868237+YazilimciNuri@users.noreply.github.com>
2026-01-03 13:16:54 +0300

    Implement basic terminal calculator functionality

---
+++ Terminal Calculator.py
@@ -0,0 +1,38 @@
+def calculator():
+    print("Python Calculator")
+    print("Operations: +  -  *  /")
+    
+
+    while True:
+        operation = input("Choose an operation (+, -, *, /) or : ").strip()
+        
+
+        if operation not in {"+", "-", "*", "/"}:
+            print("Invalid operation.\n")
+            continue
+
+        try:
+            x = float(input("Enter first number: "))
+            y = float(input("Enter second number: "))
+
+            if operation == "+":
+                result = x + y
+            elif operation == "-":
+                result = x - y
+            elif operation == "*":
+                result = x * y
+            elif operation == "/":
+                if y == 0:
+                    print("Error: Division by zero.\n")
+                    continue
+                result = x / y
+
+            print(f"Result: {result}\n")
+
+        except ValueError:
+            print("Please enter valid numbers.\n")
+
+
+if __name__ == "__main__":
+    calculator()
+
